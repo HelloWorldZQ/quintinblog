e3b2664aaefb57f88b8c1cd42a178fa1f1919d1d
quintin.zhang@foxmail.com <quintin.zhang@foxmail.com>
2018-07-30 23:04:56 +0800

    update

--- theme/themes.go
+++ theme/themes.go
@@ -21,9 +21,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/b3log/pipe/log"
-	"github.com/b3log/pipe/util"
-	"github.com/b3log/pipe/model"
+	"github.com/HelloWorldZQ/quintinblog/log"
+	"github.com/HelloWorldZQ/quintinblog/util"
+	"github.com/HelloWorldZQ/quintinblog/model"
 )
 
 // Logger
